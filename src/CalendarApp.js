194cce3e323642286daf50c81d67c1780abbf1a9
erc83 <espinoza.eric@gmail.com>
2023-02-24 12:40:55 -0300

    routes, screen

---
+++ src/CalendarApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import CalendarScreen from './components/calendar/CalendarScreen'
+import AppRouter from "./router/AppRouter";
+
+const CalendarApp = () => {
+  return (
+    <div>
+        <h1><AppRouter /> </h1>
+    </div>
+  )
+}
+
+export default CalendarApp
---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import AppRouter from '../../router/AppRouter'
+
+const CalendarScreen = () => {
+  return (
+    <div>
+        <h1>Hola Calendar app</h1>
+    </div>
+  )
+}
+
+export default CalendarScreen
