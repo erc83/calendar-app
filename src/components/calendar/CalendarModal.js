7e05d77cc1ce5e952b0d96f36823e349ef9899ea
erc83 <espinoza.eric@gmail.com>
2023-02-25 20:47:27 -0300

    form validation

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import Modal from 'react-modal';
 import DateTimePicker from 'react-datetime-picker';
+import Swal from 'sweetalert2'                      //4 instalación de sweet alert 2
 
 const customStyles = {  
     content: {
@@ -19,11 +20,11 @@ Modal.setAppElement('#root');
 const now = moment().minutes(0).seconds(0).add(1, 'hours'); 
 const nowPlus1 = now.clone().add( 1, 'hours' );
 
-
 export const CalendarModal = () => {
 
     const [ dateStart, setDateStart ] = useState( now.toDate() );
     const [ dateEnd, setDateEnd ] = useState( nowPlus1.toDate() );
+    const [titleValid, setTitleValid] = useState( true )      //7   si el titulo esta correcto es true y cuando no sea correcto se llama setTitleValid
 
     const [ formValues, setFormValues ] = useState({    
       title: 'evento',
@@ -32,7 +33,7 @@ export const CalendarModal = () => {
       end: nowPlus1.toDate()
     })
 
-    const { notes, title } = formValues;   
+    const { notes, title, start, end } = formValues;    //1 se agrega start y end para trabajar de mejor manera la fecha con moment  
     
     const handleInputChange = ({ target }) => {  
 
@@ -50,8 +51,6 @@ export const CalendarModal = () => {
         ...formValues,
         start: e 
       })
-
-
     }
 
     const handleEndDateChange = ( e ) => {
@@ -67,19 +66,39 @@ export const CalendarModal = () => {
     const closeModal = () => {
         console.log('closing...')
         //setIsOpen( false )
+        //TODO: cerrar el modal                 //12 falta agregar la configuración
     }
 
     const handleSubmitForm = (e) => {     
       e.preventDefault();
-      console.log( formValues )
-    }
+      //console.log( formValues )
 
+      const momentStart = moment( start );        //2 se tranforman las intacias date de javascript a instancias de moment
+      const momentEnd = moment( end );
+      //console.log(momentStart)
+      //console.log(momentEnd)
+
+      
+      if(momentStart.isSameOrAfter( momentEnd ) ){   //3 si la fecha de inicializacion es igual o esta despues de la fecha end es un error
+        //console.log('Fecha 2 debe de ser mayor')
+        Swal.fire('Error', 'La fecha fin debe de ser mayor a la fecha de inicio', 'error') //5
+        return;
+      }
+
+      if( title.trim().length < 2 ){                //6 validacion de la caja de texto con bootstrap otra forma de hacerlo
+        //return;                                      
+        return setTitleValid( false );              //8 
+      }
+
+      //TODO: falta realizar la grabacion en la base de datos
+
+      setTitleValid(true);                          //11 para que se quite la caja roja con el submit del formulario ok
+      closeModal();
+    }
 
   return (
     <Modal
-        //isOpen={ isOpen }  // si esta en true se muestra si esta en false se oculta
         isOpen={ true }
-        // onAfterOpen={afterOpenModal}
         onRequestClose={ closeModal }
         style={ customStyles }
         closeTimeoutMS={ 200 }
@@ -118,7 +137,8 @@ export const CalendarModal = () => {
                 <label>Titulo y notas</label>
                 <input 
                     type="text" 
-                    className="form-control"
+                    //className="form-control is-valid"                               //9 se trabaja  con la clase is-valid y is-invalid
+                    className={ `form-control ${ !titleValid && 'is-invalid'} `}      //10 muestra la caja con rojo con el detalle
                     placeholder="Título del evento"
                     name="title"
                     autoComplete="off"
