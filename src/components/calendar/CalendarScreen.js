1ce6e859bdc739b9698f4f79299a46d5bcc35bf1
erc83 <espinoza.eric@gmail.com>
2023-02-25 01:01:20 -0300

    configuracion adicional calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import 'moment/locale/es'    // para cambiar el idioma en moment
 
 import Navbar from '../ui/Navbar'
+import { messages } from '../helpers/calendar-messages-es'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+moment.locale('es');    // para cambiar el idioma en moment de los dias y los meses
 
 const localizer = momentLocalizer(moment);
 
@@ -11,10 +15,29 @@ const myEventsList = [{
   title: 'Cumpleaños de Elías',
   start: moment().toDate(),    //new Date()
   end: moment().add( 2, 'hours').toDate(),
-  bgcolor: '#fafafa'
+  bgcolor: '#fafafa',
+  notes: 'Comprar el Pastel'
 }]
 
 const CalendarScreen = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    // console.log(event, start, end, isSelected)
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  
+  };
+ 
+
   return (
    <div className='calendar-screen'>
         <Navbar />
@@ -24,6 +47,8 @@ const CalendarScreen = () => {
           events={ myEventsList }
           startAccessor="start"
           endAccessor="end"
+          messages={ messages }
+          eventPropGetter={ eventStyleGetter }
         />
    </div> 
   
